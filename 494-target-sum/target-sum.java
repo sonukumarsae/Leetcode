2dabb0615da29ef345fe7504869ada71f42d4520
sonukumarsae <sonukumar.sae.comp@gmail.com>
2026-09-15 05:17:20 +0530

    Time: 7 ms (65.35%) | Memory: 47.1 MB (14.06%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,20 +1,29 @@
 class Solution {
-    public int Count(int nums[],int target,int currSum,int i){
+    public int Count(int nums[],int target,int currSum,int i,int dp[][],int totalsum){
         if(i<0 && target==currSum){
             return 1;
         }
         if(i<0){
             return 0;
         }
-        
-           int a= Count(nums,target,currSum+nums[i],i-1);
-            int b=Count(nums,target,currSum-nums[i],i-1);
-            return a+b;
+        if(dp[i][currSum+totalsum]!=-1){
+            return dp[i][currSum+totalsum];
+        }
+           int a= Count(nums,target,currSum+nums[i],i-1,dp,totalsum);
+            int b=Count(nums,target,currSum-nums[i],i-1,dp,totalsum);
+            return dp[i][currSum+totalsum]= a+b;
         
       
     }
     public int findTargetSumWays(int[] nums, int target) {
-       
-        return Count(nums,target,0,nums.length-1);
+        int totalsum=0;
+        for(int a:nums){
+            totalsum+=a;
+        }
+       int dp[][]=new int[nums.length][2*totalsum+1];
+       for(int d[] : dp){
+        Arrays.fill(d,-1);
+       }
+        return Count(nums,target,0,nums.length-1,dp,totalsum);
     }
 }
