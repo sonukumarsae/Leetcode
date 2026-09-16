5a460a9267c093340337cb7d1c5534ebe2a91f47
sonukumarsae <sonukumar.sae.comp@gmail.com>
2026-09-16 21:24:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (47.55%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int count(int m,int n,int maze[][],int i, int j,int dp[][]){
+    public int count(int m,int n,int i, int j,int dp[][]){
         if(i==m || j==n){
             return 0;
         }
@@ -9,17 +9,17 @@ class Solution {
         if(i==m-1 && j==n-1){
             return 1;
         }
-        int a=count(m,n,maze,i+1,j,dp);
-        int b=count(m,n,maze,i,j+1,dp);
+        int a=count(m,n,i+1,j,dp);
+        int b=count(m,n,i,j+1,dp);
         return dp[i][j]= a+b;
 
     }
     public int uniquePaths(int m, int n) {
-        int maze[][]=new int[m][n];
+      
         int dp[][]=new int[m][n];
         for(int d[]:dp){
             Arrays.fill(d,-1);
         }
-        return count(m,n,maze,0,0,dp);
+        return count(m,n,0,0,dp);
     }
 }
