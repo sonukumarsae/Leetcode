1f89b51d7f70600aee39a22831d7830cd20e609a
sonukumarsae <sonukumar.sae.comp@gmail.com>
2026-09-22 17:53:58 +0530

    Time: 1 ms (99.93%) | Memory: 45.1 MB (88.98%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int compress(char[] chars) {
+        //StringBuilder sb=new StringBuilder("");
+        if(chars.length==1){
+            return 1;
+        }
+        int j=0;
+        for(int i=0;i<chars.length;i++){
+            char curr=chars[i];
+            int count=1;
+            
+           while(i<chars.length-1 && curr==chars[i+1]){
+                count++;
+                i++;
+           }
+           if(count>1){
+            chars[j++]=curr;
+            
+            String countstr=String.valueOf(count);
+            char digits[]=countstr.toCharArray();
+            for(char c:digits){
+                chars[j++]=c;
+            }
+           }
+           else{
+            chars[j++]=curr;
+            
+           }
+            
+        }
+        return j;
+    }
+}
